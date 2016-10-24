ab2e298bd00fdd4da9d6b33ffeb50875d025628e
tevistumulty <tevis.tumulty@west-mec.org>
2016-10-24 11:27:39 -0700

    Add files via upload

---
+++ js/errorMessages.js
@@ -0,0 +1,16 @@
+// Script 10.1 - errorMessages.js
+// This script defines functions for adding and removing error messages.
+
+// This function adds the error message.
+// It takes two arguments: the form element ID and the message.
+function addErrorMessage(id, msg) {
+   	'use strict';
+    
+} // End of addErrorMessage() function.
+
+// This function removes the error message.
+// It takes one argument: the form element ID.
+function removeErrorMessage(id) {
+   	'use strict';
+
+} // End of removeErrorMessage() function.
---
+++ js/register.js
@@ -0,0 +1,23 @@
+// Script 10.7- register.js
+// This script validates a form.
+
+// Function called when the form is submitted.
+// Function validates the form data.
+
+function validateForm(e) {
+    'use strict';
+    
+} // End of validateForm() function.
+
+// Function called when the terms checkbox changes.
+// Function enables and disables the submit button.
+function toggleSubmit() {
+	'use strict';
+    
+} // End of toggleSubmit() function.
+
+// Establish functionality on window load:
+window.onload = function() {
+    'use strict';
+
+};
